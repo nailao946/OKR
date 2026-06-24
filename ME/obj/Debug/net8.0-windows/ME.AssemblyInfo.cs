9f50ef927ddb48c838ce381608d45baa8c3d3b22
nailao946 <1499964238@qq.com>
2026-06-24 23:12:23 +0800

    v1.2.9: fix InputTextBoxStyle template - text visible in all dialogs

--- ME/obj/Debug/net8.0-windows/ME.AssemblyInfo.cs
+++ ME/obj/Debug/net8.0-windows/ME.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JOE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4246400ef38b136dad031779093f11c212d8388e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ca98051603bb8695132ea8237cfd8cdbe70c759")]
 [assembly: System.Reflection.AssemblyProductAttribute("ME")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ME")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
